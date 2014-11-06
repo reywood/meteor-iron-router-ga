bfe3fe42ea7b3d950893b3882b6e7f74891fa794
bitomule <bitomule@gmail.com>
2014-11-06 13:16:46 +0100

    Update iron router to 1.0.0
    Right now is using an old version.

--- package.js
+++ package.js
@@ -1,13 +1,13 @@
 Package.describe({
     name: "reywood:iron-router-ga",
     summary: "Google analytics (universal edition) with some Iron Router sugar for tracking page views.",
-    version: "0.3.2",
+    version: "0.3.3",
     git: "https://github.com/reywood/meteor-iron-router-ga.git"
 });
 
 Package.onUse(function(api) {
     api.versionsFrom("METEOR@0.9.0");
-    api.use("iron:router@0.9.1", "client");
+    api.use("iron:router@1.0.0", "client");
 
     api.addFiles([ "lib/ga.js", "lib/router.js" ], "client");
 });
