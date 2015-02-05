b5b9da79d6a022f8d5c0046ebbed610b0f170df8
Sean Dwyer <sean.dwyer@gmail.com>
2015-02-05 14:55:05 -0800

    Remove unnecessary "name" args to functions

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
     name: "reywood:iron-router-ga",
     summary: "Google analytics (universal edition) with some Iron Router sugar for tracking page views.",
-    version: "0.5.0",
+    version: "0.5.1",
     git: "https://github.com/reywood/meteor-iron-router-ga.git"
 });
 
