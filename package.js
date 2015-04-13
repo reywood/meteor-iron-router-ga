deed963071442a250ac328fe4f04929ec8e74bca
Sean Dwyer <sean.dwyer@gmail.com>
2015-04-13 15:41:19 -0700

    Automatically add BrowserPolicy rules if browser-policy package is present

--- package.js
+++ package.js
@@ -1,17 +1,21 @@
 Package.describe({
-    name: "reywood:iron-router-ga",
-    summary: "Google analytics (universal edition) with some Iron Router sugar for tracking page views.",
-    version: "0.5.3",
-    git: "https://github.com/reywood/meteor-iron-router-ga.git"
+    name: 'reywood:iron-router-ga',
+    summary: 'Google analytics (universal edition) with some Iron Router sugar for tracking page views.',
+    version: '0.6.0',
+    git: 'https://github.com/reywood/meteor-iron-router-ga.git'
 });
 
 Package.onUse(function(api) {
-    api.versionsFrom("METEOR@1.0.1");
-    api.use([ "templating", "iron:router@1.0.0" ], "client");
+    api.versionsFrom('METEOR@1.0.1');
+    api.use([ 'templating', 'iron:router@1.0.0' ], 'client');
 
     api.addFiles([
-        "lib/head.html",
-        "lib/ga.js",
-        "lib/router.js"
-    ], "client");
+        'lib/head.html',
+        'lib/ga.js',
+        'lib/router.js'
+    ], 'client');
+
+    api.addFiles([
+        'lib/browser_policy.js'
+    ], 'server');
 });
