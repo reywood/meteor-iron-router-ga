4f7fbbaa370b8d9c6fcb15c7977d6985e45db43c
Sean Dwyer <sean.dwyer@gmail.com>
2017-05-11 23:28:37 -0700

    Publish v2.0.1

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
     name: 'reywood:iron-router-ga',
     summary: 'Google analytics (universal edition) with some Iron Router sugar for tracking page views.',
-    version: '2.0.0',
+    version: '2.0.1',
     git: 'https://github.com/reywood/meteor-iron-router-ga.git',
 });
 
